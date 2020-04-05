6921d76c39ae87af1fb4a8ce36c19b0b43304e6f
joankuah <joankuah@hotmail.com>
2020-04-05 19:42:59 +0800

    Exercise 1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Tutorial</h1>
+    <exercise1/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Exercise1 from './components/Exercise1.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Exercise1
   }
 }
 </script>
---
+++ src/components/Exercise1.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Exercise 1</h1>
+    <p>VueJS is pretty cool - {{ name }} ({{ age }})</p>
+    <hr>
+    <p>My age mulitipled by 3 is {{ ageMultipled() }}</p>
+    <hr>
+    <p>RANDOM NUMBR BETWEEN 0 AND 100: {{ randomNumber(0,100) }}</p>
+    <hr>
+    <div>
+      <img :src="imageLink">
+    </div>
+    <hr>
+    <div>
+      <input type="text" :value="name">
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Joan',
+    age: 20,
+    imageLink : 'https://techcrunch.com/wp-content/uploads/2015/10/screen-shot-2015-10-08-at-4-20-16-pm.png?w=730&crop=1'
+  }),
+  methods: {
+    ageMultipled() {
+      return this.age*3
+    },
+    randomNumber(min, max) {
+      return Math.floor(Math.random(min, max) * Math.floor(max))
+    }
+  }
+}
+</script>
